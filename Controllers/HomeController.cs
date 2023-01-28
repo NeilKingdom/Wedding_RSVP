4edbf37c418f93a92b62ec485ce7b4421b3d54b2
NeilKingdom <neil@neilkingdom.xyz>
2023-01-28 17:13:26 -0500

    Almost done all css. Committing before changing carousel buttons

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,19 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
-using Wedding_RSVP.Data;
-using Wedding_RSVP.Models;
-using Wedding_RSVP.Models.ViewModels;
-using HtmlAgilityPack;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
-using System.Net;
-using System.Text;
-using System.IO;
+using System.Runtime;
 
 namespace Wedding_RSVP.Controllers
 {
    public class HomeController : Controller
    {
-      public IActionResult Index() => View();
+      private const string _weddingDate = "08/26/2023";
+      private DateTime _weddingDateTime;
+
+      public HomeController()
+      {
+         _weddingDateTime = Convert.ToDateTime(_weddingDate);
+         ViewBag.Date = _weddingDate;
+      }
+
+      public IActionResult Index()
+      {
+         ViewBag.Date = _weddingDate;
+         return View();
+      }
+
+      public IActionResult SwapDate()
+      {
+         var currDate = DateTime.Now;
+         if (ViewBag.Date == _weddingDate) // Display as a countdown
+            ViewBag.Date = "Days Remaining: " + (int)(_weddingDateTime - currDate).TotalDays;
+         else // Display in date format
+            ViewBag.Date = _weddingDate;
+
+         return View(nameof(Index));
+      }
    }
 }
